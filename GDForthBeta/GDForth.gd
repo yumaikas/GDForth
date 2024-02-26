ff7ed4064faf81e2ee7e5e1cfa622fe751b49774
Andrew Owen <yumaikas94@gmail.com>
2024-02-26 05:07:54 -0700

    Tracing, non-VM ustack, 'str > :str, interp-args
    - Allow trace output to be customized
    - Implement the Utility stack inside of the GDForth stdlib, instead of in the VM
    - Switch string prefix char to : from '
    - Set up additional arguments for interpret calls

--- GDForthBeta/GDForth.gd
+++ GDForthBeta/GDForth.gd
@@ -2,21 +2,20 @@ class_name GDForth extends Reference
 
 const StackFrame = preload("./StackFrame.gd")
 
-var MEM = []; 
-var RS = [] # Add the bottom frame in _init
+var MEM = []; var RS = [] # Add the bottom frame in _init
 var word = ""; var pos = 0; var INPUT = ""; # The current input string
 var inbox = []
 
-var trace = 0; var trace_indent
-var stack = []; var util_stack = []; var compile_stack = [MEM]
-var locals = [];
-var constants = {};
+var trace = 0;  var trace_modes = ["mode", "instr", "stacklen", "pos"]; var trace_indent; var trace_stack = []
+var stack = []; var compile_stack = [MEM]
+var locals = []; var constants = {};
 var head; var fatal_err;
 var mode = "m_interpret"
 
 func sfr(name): return funcref(self, name)
 	
 var dict = { "&": sfr("mapfn") }; var heads = { "DOCOL": funcref(self, "h_DOCOL") }
+var name_dict = {}
 var IMMEDIATE = { "];": true, "[": true, "]": true }
 
 func fault(m0, m1="", m2="",m3="", m4="", m5="",m6="",m7="",m8=""):
@@ -26,57 +25,75 @@ func fault(m0, m1="", m2="",m3="", m4="", m5="",m6="",m7="",m8=""):
 	mode="stop"
 	return message
 
+func vm(): _push(self)
 func hcf(m0, m1="", m2="", m3="", m4="", m5="", m6="", m7="", m8=""):
 	fatal_err = fault(m0,m1,m2,m3,m4,m5,m6,m7,m8)
 
+# :const :defconst &  
 var stdlib = """
-'clear-stack '_clearstack & 'stack-size '_stacklen & 
-'u> '_u_pop_tos & 'u< '_u_push_tos & 'call-method 'call_method &
-'LIT 'LIT & 'EXIT '_exit & '?REDO-BLOCK 'reset_block_if &
-'? 'cond_pick & '[ 'begin_block & '] 'end_block & 'exec 'exec &
-'def[ 'def & ']; 'exit_def & 'const 'defconst &  
-'here '_here & 'assert '_assert &  
-'eq? 'op_eq & 'lt? 'op_lt & 'gt? 'op_gt &
-'narray '_narray & '+ 'op_add & '- 'op_sub & 
-'+trace '_trace_inc & '-trace '_trace_dec &
-'pick-del '_pick_del & '_s '_printstack &
+:VM :vm & :get :dget & :put :dput & :<dict> :newdict &
+:clear-stack :_clearstack & :stack-size :_stacklen & 
+:call-method :call_method & :LIT :LIT & :EXIT :_exit & :?REDO-BLOCK :reset_block_if &
+:? :cond_pick & :[ :begin_block & :] :end_block & :exec :exec &
+:def[ :def & :]; :exit_def & 
+:here :_here & :assert :_assert &  
+:eq? :op_eq & :lt? :op_lt & :gt? :op_gt &
+:narray :_narray & :+ :op_add & :- :op_sub & 
+:pick-del :_pick_del & :_s :_printstack &
+:+trace  :_trace_inc & :-trace :_trace_dec & :untrace :_untrace & :retrace :_retrace &
 
-'{  def[ stack-size u< ]; '}  def[ stack-size u> - narray ];
+:<arr> def[ 0 narray ]; :box def[ 1 narray ]; :pair def[ 2 narray ]; :triple def[ 3 narray ];
+:spin def[ untrace -1 -3 -2 triple -1 -1 -1 triple pick-del retrace ];
+:vmget def[ box VM :get call-method ]; :vmset def[ pair VM :set call-method ];
+:const def[ untrace :constants vmget spin put retrace ];
 
-'false 0 1 eq? const 'true 1 1 eq? const
+:util-stack <arr> const
+:u< def[ untrace 1 narray util-stack :append call-method retrace ]; 
+:u> def[ untrace 0 narray util-stack :pop_back call-method retrace ];
 
-'{} { } const '{-1} { -1 } const '{-2} { -2 } const
-'{-1,-1} { -1 -1 } const '{-1,-2} { -1 -2 } const
-'{spin-pick} { -1 -3 -2 } const '{spin-del} { -1 -1 -1 } const
+:{  def[ stack-size u< ]; :}  def[ stack-size u> - narray ];
+:{} <arr> const :{-1} -1 box const :{-2} -2 box const
 
-'[] [ ] const
-'if def[ [] ? exec ]; 'if-else def[ ? exec ];
+:1+ def[ 1 + ]; :1- def[ 1 - ]; :nom def[ u> 1- u< ];
+:( def[ u< u< { ]; :) def[ } u> u> call-method ];
+:() def[ u< u< {} u> u> call-method ];
+:false 0 1 eq? const :true 1 1 eq? const
 
-'dup  def[ {-1} {} pick-del ];
-'2dup def[ {-1,-2} {} pick-del ];
-'drop def[ {} {-1} pick-del ];
-'1+ def[ 1 + ];
-'spin def[ {spin-pick} {spin-del} pick-del ];
-'drop def[ {} {-1} pick-del ];
-'nip  def[ {} {-2} pick-del ];
-'not def[ false true ? ];
-'ucopy def[ u> dup u< ];
-'udrop def[ u> drop ];
-'while def[ u< [ ucopy exec ?REDO-BLOCK udrop ] exec ];
+:[] [ ] const
+:if def[ [] ? exec ]; :if-else def[ ? exec ];
+
+:dup def[ {-1} {} pick-del ];
+:{-1,-2} -1 -2 pair const
+:2dup def[ {-1,-2} {} pick-del ];
+:drop def[ {} {-1} pick-del ];
+
+:{spin-pick} { -1 -3 -2 } const :{spin-del} { -1 -1 -1 } const
+:spin def[ untrace {spin-pick} {spin-del} pick-del retrace ];
+:drop def[ {} {-1} pick-del ]; :nip def[ {} {-2} pick-del ];
+:not def[ false true ? ];
+:ucopy def[ u> dup u< ]; :udrop def[ u> drop ];
+
+:while def[ u< [ ucopy exec ?REDO-BLOCK udrop ] exec ];
+:swap def[ untrace {-2} {-2} pick-del retrace ];
+:bind def[ :self swap const ];
 """
 
 func _here(): _push(len(MEM))
-func _pop(): return stack.pop_back()
+func _pop(): 
+	if not stack.empty():
+		return stack.pop_back()
+	hcf("data underflow")
+	return null
+
 func _drop(): _pop()
 func _push(val): stack.push_back(val)
-func _u_pop(): return util_stack.pop_back()
-func _u_push(val): util_stack.push_back(val)
-func _u_push_tos(): _u_push(_pop())
-func _u_pop_tos(): _push(_u_pop())
 func _printstack(): print(stack);
 func _stacklen(): _push(len(stack))
 func _trace_inc(): trace+=1
 func _trace_dec(): trace-=1
+func _untrace():
+	trace_stack.push_back(trace); trace=0
+func _retrace(): trace = trace_stack.pop_back()
 
 func _assert():
 	var msg = _pop()
@@ -200,8 +217,10 @@ func _exit():
 		mode = IP()
 		IP_pop()
 
+var lastlit
 func LIT(): 
-	stack.append(instr()); IP_inc()
+	lastlit = instr()
+	stack.append(lastlit); IP_inc()
 
 func compile(val): compile_stack.back().append(val)
 func compile_many(vals): compile_stack.back().append_array(vals)
@@ -250,9 +269,33 @@ func run():
 		if oldmode != "m_forth":
 			inst = word
 		if trace > 0:
-			prints("mode:", oldmode, " inst:", inst, " Stack: ",  stack, " RS:", RS, "US:", util_stack, "pos:", pos)
+			for t in trace_modes:
+				if _eq(t, "mode"):
+					printraw(" mode: ", oldmode)
+				elif _eq(t, "instr"):
+					printraw(" inst: ", inst)
+					if _eq(inst, "LIT"):
+						printraw(" <<", lastlit, ">> ")
+					elif typeof(inst) == TYPE_INT:
+						printraw(" [", name_dict[inst], "]")
 
-func interpret(input: String):
+				elif _eq(t, "stack"):
+					printraw(" stack: ", stack)
+				elif _eq(t, "stacklen"):
+					printraw(" len(stack): ", len(stack))
+				elif _eq(t, "RS"):
+					printraw(" RS ", RS)
+				elif typeof(t) == TYPE_ARRAY and t[0] == "consts":
+					for c in t.slice(1, -1):
+						printraw(" ", c, ": ", constants[c])
+			print()
+
+func interpret(input: String, s0=null, s1=null, s2=null, s3=null, s4=null, s5=null, s6=null, s7=null, s8=null, s9=null):
+	var args = [s0,s1,s2,s3,s4,s5,s6,s7,s8,s9]
+	for a in args: 
+		if not _eq(a, null):
+			_push(a)
+
 	INPUT = input; pos = 0
 	mode = "m_interpret"
 	# if input != stdlib:
@@ -319,7 +362,7 @@ func interpret_literal():
 		stack.append(int(word)); return true
 	elif word.is_valid_float():
 		stack.append(float(word)); return true
-	elif word.begins_with("'"):
+	elif word.begins_with(":"):
 		stack.append(word.substr(1)); return true
 	elif word.begins_with("[["):
 		pos = pos - len(word) + 2
@@ -367,7 +410,7 @@ func compile_literal():
 		compile_many(["LIT", float(word)]); return true
 	elif word.is_valid_integer():
 		compile_many(["LIT", int(word)]); return true
-	elif word.begins_with("'"):
+	elif word.begins_with(":"):
 		compile_many(["LIT", word.substr(1)]); return true
 	elif word.begins_with("[["):
 		var maybeStr = scan_for_seq("]]")
@@ -378,55 +421,61 @@ func compile_literal():
 	return false
 
 func mapfn():
-	var _val = _pop()
-	var _key = _pop()
-
+	var _val = _pop(); var _key = _pop()
 	dict[_key] = sfr(_val)
 
-func defconst():
+func newdict(): _push({})
+func dget():
+	var _key = _pop()
+	var _dict = _pop()
+	if _dict.has(_key):
+		_push(_dict)
+	else:
+		_push(null)
+
+func dput():
 	var _val = _pop()
 	var _key = _pop()
-	constants[_key] = _val
+	var _dict = _pop() #print(_dict, _key, _val)
+	_dict[_key] = _val
 
 func def():
 	if mode == "m_compile":
-		hcf("Cannot nest word defs!")
-		return
+		hcf("Cannot nest word defs!"); return
 
 	var name = _pop()
-	dict[name] = len(MEM)
+	dict[name] = len(MEM); name_dict[len(MEM)] = name
 	compile("DOCOL") #compile(["NAME:", name])
 	mode = "m_compile"
 
 func exit_def():
-	compile("EXIT")
-	mode = "m_interpret"
+	compile("EXIT"); mode = "m_interpret"
 
 func call_method(push_nulls=false):
-	var on = _pop(); var name = _pop(); var margs = _pop().duplicate()
-	print(on, name, margs, push_nulls)
+	# margs on name
+	var name = _pop(); var on = _pop(); var margs = _pop().duplicate()
+	# print(on, name, margs, push_nulls)
 	_dispatch(on, name, margs, push_nulls)
 
 const argNames = ["a0", "a1", "a2", "a3", "a4", "a5", "a6", "a7", "a8", "a9"]
-
 func _dispatch(on, name, margs, push_nulls = false):
 	if typeof(on) == TYPE_OBJECT:
-		var fn = funcref(on, name)
-		var ret = fn.call_funcv(margs)
-		on.callv(name, margs)
+		var ret = on.callv(name, margs)
 		if ret != null or push_nulls:
 			_push(ret)
 	else:
 		var expr = Expression.new()
-		var anames = argNames.slice(0, len(margs))
-		anames.append("m")
+		var anames = argNames.slice(0, len(margs)-1)
+		if len(margs) == 0:
+			anames = []
 		var toParse = str("m.", name,"(", ", ".join(anames), ")")
+		anames.append("m")
 		if expr.parse(toParse, anames) != OK:
 			hcf("Unable to parse: ", toParse, anames, " as an expression")
 			return
 
 		margs.append(on)
-		if trace > 0:
+		if trace > 0: 
 			print(toParse, anames, margs)
 		var ret = expr.execute(margs)
 
--- GDForthBeta/Tests.gd
+++ GDForthBeta/Tests.gd
@@ -5,6 +5,19 @@ const GDForth = preload("./GDForth.gd")
 func _init():
 	for i in 5:
 		print()
+	var focus = false
+	for m in get_method_list():
+		if m.name.begins_with("__test"):
+			focus = true
+			var comp = GDForth.new()
+			printraw(m.name, ": ")
+			callv(m.name, [comp])
+			print()
+
+	if focus:
+		call_deferred('quit')
+		return
+
 	for m in get_method_list():
 		if m.name.begins_with("test_"):
 			var comp = GDForth.new()
@@ -14,64 +27,6 @@ func _init():
 			if !comp.done:
 				break
 	call_deferred('quit')
-
-func test_pushing(comp):
-	comp.interpret("'asdf")
-	assert_(comp.stack.back() == "asdf", "String Symbol Literal failed!")
-
-	comp.interpret(" [[this is a test]] ")
-	assert_(comp.stack.back() == "this is a test", "Long string failed!")
-
-	comp.interpret("clear-stack")
-	comp.interpret("1 2 3")
-	assert_(comp.stack == [1, 2, 3], "Can push integers")
-
-	comp.interpret("clear-stack 11 23 354")
-	assert_(comp.stack == [11, 23, 354], "Can push bigger integers")
-
-	comp.interpret("1 u< 2 u<")
-	assert_(array_eq(comp.util_stack, [1, 2]), "Can push to check-stack")
-
-func test_compilation(comp):
-	comp.interpret("'one def[ 1 ];")
-	assert_(
-		array_eq(comp.MEM.slice(-4, -1), [ "DOCOL", "LIT", 1, "EXIT" ]),
-		"Compile number")
-	comp.interpret("'add def[ + ];") 
-	assert_( array_eq(comp.MEM.slice(-3,-1), [ "DOCOL", "+", "EXIT" ]), "Compile primitive") 
-	comp.interpret("one one add") 
-	assert_( array_eq(comp.stack, [ 2 ]), "Execute compiled nonprimitives")
-
-func test_numbers(comp):
-	comp.interpret("-1 -2 -3")
-	assert_(array_eq(comp.stack, [-1, -2, -3]), "Negative Numbers Work")
-
-func test_ops(comp):
-	comp.interpret("1 2 lt? 2 1 gt? 0 0 eq?")
-	assert_(array_eq(comp.stack, [true, true, true]), "Ops")
-
-
-func test_blocks(comp):
-	comp.interpret("{ 1 2 3 }")
-	assert_(array_eq(comp.stack.back(), [1,2,3]), "Basic arrays")
-	comp.interpret("clear-stack")
-
-	comp.interpret("[ 1 2.0 + ] exec")
-	stack_assert(comp, [3.0], "Can exec")
-
-
-	comp.interpret("'2max def[ 2dup lt? spin ? ];")
-	comp.interpret("1 2 2max")
-	stack_assert(comp, [2], "2max worked")
-
-func test_looping(comp):
-
-	comp.interpret("1 2 3 [ 1 + false ] while")
-	stack_assert(comp, [1,2,4], "While terminates")
-
-	comp.interpret("0 [ 1+ dup 4 lt? ] while")
-	stack_assert(comp, [4], "Counted While")
-
 
 func stack_assert(comp, matches, msg):
 	if not array_eq(comp.stack, matches):
@@ -99,3 +54,77 @@ func assert_(cond, message):
 		quit()
 	else:
 		printraw(".")
+
+func test_pushing(comp):
+	comp.interpret(":asdf")
+	assert_(comp.stack.back() == "asdf", "String Symbol Literal failed!")
+
+	comp.interpret(" [[this is a test]] ")
+	assert_(comp.stack.back() == "this is a test", "Long string failed!")
+
+	comp.interpret("clear-stack")
+	comp.interpret("1 2 3")
+	assert_(comp.stack == [1, 2, 3], "Can push integers")
+
+	comp.interpret("clear-stack 11 23 354")
+	assert_(comp.stack == [11, 23, 354], "Can push bigger integers")
+
+	comp.interpret("1 u< 2 u<")
+	assert_(array_eq(comp.constants['util-stack'], [1, 2]), "Can push to util-stack")
+
+func test_compilation(comp):
+	comp.interpret(":one def[ 1 ];")
+	assert_(
+		array_eq(comp.MEM.slice(-4, -1), [ "DOCOL", "LIT", 1, "EXIT" ]),
+		"Compile number")
+	comp.interpret(":add def[ + ];") 
+	assert_( array_eq(comp.MEM.slice(-3,-1), [ "DOCOL", "+", "EXIT" ]), "Compile primitive") 
+	comp.interpret("one one add") 
+	assert_( array_eq(comp.stack, [ 2 ]), "Execute compiled nonprimitives")
+
+func test_numbers(comp):
+	comp.interpret("-1 -2 -3")
+	assert_(array_eq(comp.stack, [-1, -2, -3]), "Negative Numbers Work")
+
+func test_ops(comp):
+	comp.interpret("1 2 lt? 2 1 gt? 0 0 eq?")
+	assert_(array_eq(comp.stack, [true, true, true]), "Ops")
+
+
+func test_blocks(comp):
+	comp.interpret("{ 1 2 3 }")
+	assert_(array_eq(comp.stack.back(), [1,2,3]), "Basic arrays")
+	comp.interpret("clear-stack")
+
+	comp.interpret("[ 1 2.0 + ] exec")
+	stack_assert(comp, [3.0], "Can exec")
+
+
+	comp.interpret(":2max def[ 2dup lt? spin ? ];")
+	comp.interpret("1 2 2max")
+	stack_assert(comp, [2], "2max worked")
+
+func test_looping(comp):
+
+	comp.interpret("1 2 3 [ 1 + false ] while")
+	stack_assert(comp, [1,2,4], "While terminates")
+
+	comp.interpret("0 [ 1+ dup 4 lt? ] while")
+	stack_assert(comp, [4], "Counted While")
+
+var call_count = 0
+func test_methods(comp):
+	comp.interpret("1 2 VM :op_add ()")
+	stack_assert(comp, [3], "can call VM methods")
+
+	comp.interpret(":TEST :to_lower ()")
+	stack_assert(comp, ["test"], "can call VM methods")
+
+	comp.interpret("bind", self)
+	comp.interpret("self :mocked_call () self :get ( :call_count )")
+	stack_assert(comp, [1], "Calling into test class")
+
+func mocked_call():
+	call_count += 1
+
+
